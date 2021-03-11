9ec3d29f2cec120f2543e5fe7ddbbb2a3f766acb
Dogadin <AlexanderDogadin.7.0@gmail.com>
2021-03-11 14:42:26 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+
+  const str = Math.abs(n).toString()
+  let ret = ''
+  for(let i = str.length-1; i>=0;i--){
+      ret = ret + str[i]
+  }
+  const i = +ret
+  return i
 }
+
